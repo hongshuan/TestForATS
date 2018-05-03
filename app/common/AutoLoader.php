41a12e36cd43cefdd0389fbff6a5091dd5521c7e
Hanson Li <lihsca@gmail.com>
2018-05-03 11:45:36 -0400

    change app/common/AutoLoader

--- app/common/AutoLoader.php
+++ app/common/AutoLoader.php
@@ -23,13 +23,13 @@ class AutoLoader
 
     public function loadClass($class)
     {
-        // Common\Database      => $basedir/common/Database.php
+        // App\Common\Database  => $basedir/app/Common/Database.php
         // App\Models\UserModel => $basedir/app/Models/UserModel.php
 
         $parts = explode('\\', $class);
-        $parts[0] = strtolower($parts[0]);
+        $parts[0] = strtolower($parts[0]); // App => app
 
-        $filename = $this->basedir . '/../' . implode('/', $parts) . '.php';
+        $filename = $this->basedir .'/'. implode('/', $parts) . '.php';
 
         if (file_exists($filename)) {
             require $filename;
